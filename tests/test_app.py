70c154cc3549db2c683b42038d10d8d3fb05caad
ricardojaj <rickanjos85@gmail.com>
2025-01-26 16:02:50 -0300

    Adc new tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,7 +11,7 @@ class TestShoppingList(unittest.TestCase):
         self.app.testing = True;
 
 
-    def test_adc_new_product(self):
+    def test_add_new_product(self):
         product =  {
             "productName": "PAO",
             "quantity": 1,
@@ -26,12 +26,31 @@ class TestShoppingList(unittest.TestCase):
         self.assertEqual(data['data'][0]['productName'], 'PAO')  
         self.assertEqual(data['data'][0]['quantity'], 1)    
         self.assertEqual(data['data'][0]['unitPrice'], 7) 
-  
-"""    def test_view_products(self):
-        response = self.app.get('/shopping_list/view')
+
+    def test_add_invalid_product(self):
+        product =  {
+            "productName": "",
+            "quantity": 0,
+            "unitPrice": 0
+        }
+
+        response = self.app.post('/shopping_list/add', json=product)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"message": "Invalid product data"})
+
+    def test_delete_product(self):
+        response = self.app.delete('/shopping_list/delete/35')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, [])
-"""       
+        self.assertEqual(response.json, {"message": "Product deleted successfully!"})
+    
+    def test_delete_non_existent_product(self):
+        response = self.app.delete('/shopping_list/delete/33')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json, {"message": "Product not found!"})
+    
+
+
 if __name__ == '__main__':
     unittest.main()
+
 
